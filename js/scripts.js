e8accc98dc4282fc9a8abaeadd0bb46d41386cc8
cah1r <cah1r@proton.me>
2023-08-16 21:16:43 +0200

    fixes for feedback 230808

--- js/scripts.js
+++ js/scripts.js
@@ -10,33 +10,34 @@
 
 // Back to top script
 window.onscroll = function () {
-  scrollFunction();
+    scrollFunction();
 };
 
 function scrollFunction() {
-  // Show the button after scrolling down the height of the viewport from the top
-  if (
-    document.body.scrollTop > window.innerHeight / 2 ||
-    document.documentElement.scrollTop > window.innerHeight / 2
-  ) {
-    document.getElementById("back-to-top").style.opacity = "1"; /* Make the button visible */
-  } else {
-    document.getElementById("back-to-top").style.opacity = "0"; /* Make the button transparent */
-  }
+    // Show the button after scrolling down the height of the viewport from the top
+    if (
+        document.body.scrollTop > window.innerHeight / 2 ||
+        document.documentElement.scrollTop > window.innerHeight / 2
+    ) {
+        document.getElementById("back-to-top").style.opacity = "1"; /* Make the button visible */
+    } else {
+        document.getElementById("back-to-top").style.opacity =
+            "0"; /* Make the button transparent */
+    }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 document.getElementById("back-to-top").onclick = function () {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 };
 
 var menuIcon = document.getElementById("mobile-menu");
 var mobileNav = document.getElementById("mobile-nav");
 
 menuIcon.addEventListener("click", function () {
-  menuIcon.classList.toggle("close");
-  mobileNav.classList.toggle("open");
+    menuIcon.classList.toggle("close");
+    mobileNav.classList.toggle("open");
 });
 
 ////////////////////////////////////////////////////////////////
