3ad87ed2f36afb759429880764584d87640211c9
cah1r <cah1r@proton.me>
2023-09-20 01:09:50 +0200

    fixes for feedback 230913

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,59 @@
 // Back to top script
 window.onscroll = function () {
-    scrollFunction();
+   scrollFunction();
 };
 
 function scrollFunction() {
-    // Show the button after scrolling down the height of the viewport from the top
-    if (
-        document.body.scrollTop > window.innerHeight / 2 ||
-        document.documentElement.scrollTop > window.innerHeight / 2
-    ) {
-        document.getElementById("back-to-top").style.opacity = "1"; /* Make the button visible */
-    } else {
-        document.getElementById("back-to-top").style.opacity =
-            "0"; /* Make the button transparent */
-    }
+   // Show the button after scrolling down the height of the viewport from the top
+   if (
+      document.body.scrollTop > window.innerHeight / 2 ||
+      document.documentElement.scrollTop > window.innerHeight / 2
+   ) {
+      document.getElementById("back-to-top").style.opacity = "1"; /* Make the button visible */
+   } else {
+      document.getElementById("back-to-top").style.opacity = "0"; /* Make the button transparent */
+   }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 document.getElementById("back-to-top").onclick = function () {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+   document.body.scrollTop = 0; // For Safari
+   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 };
 
-var menuIcon = document.getElementById("mobile-menu");
-var mobileNav = document.getElementById("mobile-nav");
-
-menuIcon.addEventListener("click", function () {
-    menuIcon.classList.toggle("close");
-    mobileNav.classList.toggle("open");
-});
-
 function setFullHeight() {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
+   let vh = window.innerHeight * 0.01;
+   document.documentElement.style.setProperty("--vh", `${vh}px`);
 }
 
 setFullHeight();
 
 window.addEventListener("resize", () => {
-    setFullHeight();
+   setFullHeight();
+});
+
+var menuIcon = document.getElementById("mobile-menu");
+var mobileNav = document.getElementById("mobile-nav");
+
+menuIcon.addEventListener("click", function (event) {
+   menuIcon.classList.toggle("close");
+   mobileNav.classList.toggle("open");
+   event.stopPropagation();
+});
+
+document.addEventListener("click", function (event) {
+   if (menuIcon.contains(event.target) || mobileNav.contains(event.target)) {
+      return;
+   }
+
+   if (mobileNav.classList.contains("open")) {
+      menuIcon.classList.remove("close");
+      mobileNav.classList.remove("open");
+   }
+});
+
+mobileNav.addEventListener("click", function (event) {
+   event.stopPropagation();
 });
 
 ////////////////////////////////////////////////////////////////
