0e1522216941a922336d8ec2d6726f5b32f65be7
cah1r <cah1r@proton.me>
2023-09-25 22:49:02 +0200

    fixes for feedback 230924

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,3 @@
-// Back to top script
-window.onscroll = function () {
-   scrollFunction();
-};
-
-function scrollFunction() {
-   // Show the button after scrolling down the height of the viewport from the top
-   if (
-      document.body.scrollTop > window.innerHeight / 2 ||
-      document.documentElement.scrollTop > window.innerHeight / 2
-   ) {
-      document.getElementById("back-to-top").style.opacity = "1"; /* Make the button visible */
-   } else {
-      document.getElementById("back-to-top").style.opacity = "0"; /* Make the button transparent */
-   }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-document.getElementById("back-to-top").onclick = function () {
-   document.body.scrollTop = 0; // For Safari
-   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-};
-
 function setFullHeight() {
    let vh = window.innerHeight * 0.01;
    document.documentElement.style.setProperty("--vh", `${vh}px`);
@@ -55,38 +32,3 @@ document.addEventListener("click", function (event) {
 mobileNav.addEventListener("click", function (event) {
    event.stopPropagation();
 });
-
-////////////////////////////////////////////////////////////////
-// SMOOTH SCROLLING JS IMPEMENTATION
-
-// const allLinks = document.querySelectorAll("a:link");
-// allLinks.forEach(function (link) {
-//   link.addEventListener("click", function (e) {
-//     e.preventDefault();
-//     const href = link.getAttribute("href");
-//     if (href.startsWith("#")) {
-//       const section = document.querySelector(href);
-//       section.scrollIntoView({ behavior: "smooth" });
-//     }
-//   });
-// });
-
-////////////////////////////////////////////////////////////////
-// Sticky navigation
-// const heroSectionEl = document.querySelector(".hero");
-// const observer = new IntersectionObserver(
-//   function (entries) {
-//     const ent = entries[0];
-//     if (!ent.isIntersecting)
-//       document.querySelector(".header").classList.add("sticky");
-//     if (ent.isIntersecting)
-//       document.querySelector(".header").classList.remove("sticky");
-//   },
-//   {
-//     //In the viewport
-//     root: null,
-//     treshold: 0,
-//     rootMargin: "180px",
-//   }
-// );
-// observer.observe(heroSectionEl);
